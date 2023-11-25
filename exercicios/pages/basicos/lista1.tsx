9910590cf0a3cd8fbf77b8988a912a58c366de5f
Alexandre Lima <al3xandr3sl@gmail.com>
2023-11-25 05:26:43 -0300

    16. Desafio Lista

--- exercicios/pages/basicos/lista1.tsx
+++ exercicios/pages/basicos/lista1.tsx
@@ -1,16 +1,16 @@
 export default function lista1() {
   return (
     <div>
-      <span>1,</span><br/>
-      <span>2,</span><br/>
-      <span>3,</span><br/>
-      <span>4,</span><br/>
-      <span>5,</span><br/>
-      <span>6,</span><br/>
-      <span>7,</span><br/>
-      <span>8,</span><br/>
-      <span>9,</span><br/>
-      <span>10,</span><br/>
+      <span>1,</span>
+      <span>2,</span>
+      <span>3,</span>
+      <span>4,</span>
+      <span>5,</span>
+      <span>6,</span>
+      <span>7,</span>
+      <span>8,</span>
+      <span>9,</span>
+      <span>10,</span>
     </div>
   );
 }
